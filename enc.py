c6d0f11b170651d207fcfc9eedf96990f78987b3
cas101101 <caleb.a.schwartz@gmail.com>
2023-04-07 12:09:11 -0400

    modified:   enc.py

--- enc.py
+++ enc.py
@@ -66,7 +66,7 @@ def runExample(revs: float):
 
 if __name__ == '__main__':
     try:
-        runExample(1)
+        runExample(4)
 
     except (KeyboardInterrupt, SystemExit) as exErr:
         print("Ending example.")
