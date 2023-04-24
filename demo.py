296d4bede474e9119cd39ce9f46ee3babd3031d9
cas101101 <caleb.a.schwartz@gmail.com>
2023-04-24 10:45:47 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -67,6 +67,8 @@ def set_speed(distance: float, speed: float, FWD: bool):
     target = ((distance/100)*ticks_per_rev)/(2*math.pi*(wheel_radius/100))
     print(target)
     print(myEncoders.count1)
+    left.set_drive(L_MTR,FWD,speed)
+    time.sleep(5)
     i = 0
     # normalize the speed
     speed_norm = (speed - speed_min)/(speed_max - speed_min)
