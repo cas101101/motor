71a7c25a973024a7699495b0fcd15712b36ef2ed
cas101101 <caleb.a.schwartz@gmail.com>
2023-04-24 10:56:00 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -69,20 +69,20 @@ def set_speed(distance: float, speed: float, dir: bool):
     left.set_drive(L_MTR,FWD,speed)
     time.sleep(5)
     print("done with drive")
-    i = 0
-    # normalize the speed
-    speed_norm = (speed - speed_min)/(speed_max - speed_min)
-    while(myEncoders.count1 < target - target/5): #- target/5
-        left.set_drive(L_MTR,FWD,speed)
-        #right.set_drive(R_MTR,FWD,speed)
+    # i = 0
+    # # normalize the speed
+    # speed_norm = (speed - speed_min)/(speed_max - speed_min)
+    # while(myEncoders.count1 < target - target/5): #- target/5
+    #     left.set_drive(L_MTR,FWD,speed)
+    #     #right.set_drive(R_MTR,FWD,speed)
         
-    while (myEncoders.count1 < target):
-        if(speed > speed * 0.3):
-            speed_norm = speed_norm*math.exp(-DECAY*i)
-            speed = (speed_max - speed_min)*speed_norm + speed_min
-            i = i + 1
-        left.set_drive(L_MTR,FWD,speed)
-        #right.set_drive(R_MTR,FWD,speed)
+    # while (myEncoders.count1 < target):
+    #     if(speed > speed * 0.3):
+    #         speed_norm = speed_norm*math.exp(-DECAY*i)
+    #         speed = (speed_max - speed_min)*speed_norm + speed_min
+    #         i = i + 1
+    #     left.set_drive(L_MTR,FWD,speed)
+    #     #right.set_drive(R_MTR,FWD,speed)
     left.disable()
     #right.disable()
 
