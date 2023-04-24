8706a1d77aa92e54fc3df8d35725e1c532d84458
cas101101 <caleb.a.schwartz@gmail.com>
2023-04-23 21:44:49 -0400

    Update demo.py
    created function

--- demo.py
+++ demo.py
@@ -7,14 +7,22 @@ steer.restart()
 
 steer.move_servo_position(0, 0)
 
-# +28 degrees is straight ahead
-while(True):
+def set_angle():
     angle = int(input("Enter angle: "))
     tic = time.perf_counter()
-    if (angle > -45 and angle < 45):
+    if (angle > -46 and angle < 46):
         steer.move_servo_position(0, angle+28)
+        # +28 degrees is straight ahead
     else:
         print("Sorry, angle invalid. Pick a number on the range [-45, 45]")
-    
+def set_speed():
+    pass
+
+# +28 degrees is straight ahead
+while(True):
+    tic = time.perf_counter()
+    # main code goes here
+    set_angle()
     toc = time.perf_counter()
     print(f"Finished in {toc - tic:0.4f} seconds")
+
