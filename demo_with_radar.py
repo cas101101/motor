4d64edc4f62b41f7191b595b6b872eb8413720c4
Aaron-00 <44624620+Aaron-00@users.noreply.github.com>
2023-05-02 16:56:59 -0400

    Update demo_with_radar.py

--- demo_with_radar.py
+++ demo_with_radar.py
@@ -112,7 +112,7 @@ def program():
     time.sleep(1)
     set_angle(45)
     time.sleep(1)
-    set_speed(35, 70, True)
+    set_speed(300, 70, True)
     time.sleep(1)
     #toc = time.perf_counter()
     #print(f"Finished in {toc - tic:0.4f} seconds")
